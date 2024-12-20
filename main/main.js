b1b4d7f46d2dc918876a0cb2ae317e7cb016117e
Mohamed-All <mohamed.ali76544321@gmail.com>
2024-12-20 02:59:59 +0200

    Add files via upload

---
+++ main/main.js
@@ -0,0 +1,13 @@
+
+function iphones(phone){
+   
+    const img = document.getElementById('max')
+    img.src = phone
+
+   
+}
+
+function color(color){
+    const clor = document.getElementById('container')
+clor.style.backgroundColor = color
+}
